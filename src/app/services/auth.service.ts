72b2d0b50719d6a653b6a95772ba07b246b05cd7
Robert Ndonga <40755436+robertnd@users.noreply.github.com>
2023-04-03 23:11:26 +0300

    Update auth.service.ts

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -3,6 +3,8 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 const AUTH_API = 'http://localhost:9085/api/v1/user/';
+// remote
+//const AUTH_API = 'http://44.202.227.2:9085/api/v1/user/';
 
 const httpOptions = {
   headers: new HttpHeaders({ 'Content-Type': 'application/json' })
