1d17e0224319484e8ec28669753da44501787fc6
Robert Ndonga <40755436+robertnd@users.noreply.github.com>
2023-04-03 23:14:17 +0300

    Update products-new.component.ts

--- src/app/business/crm/products-new/products-new.component.ts
+++ src/app/business/crm/products-new/products-new.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef, AfterViewInit, OnInit } from '@angular/core';
 import { StorageService } from 'src/app/services/storage.service';
 import { ViewService } from 'src/app/services/view.service';
 import Stepper from 'bs-stepper';
@@ -9,10 +9,12 @@ import { Router } from '@angular/router';
   templateUrl: './products-new.component.html',
   styleUrls: ['./products-new.component.scss']
 })
-export class ProductsNewComponent {
+export class ProductsNewComponent implements AfterViewInit, OnInit {
 
   currentUser: any;
   canView: any;
+
+  @ViewChild('bsStepper', { static: false }) stepperElement!: ElementRef<any>;
   private stepper!: Stepper;
 
   constructor(private storageService: StorageService, 
@@ -27,14 +29,17 @@ export class ProductsNewComponent {
     this.stepper.previous();
   }
 
+  ngAfterViewInit() {
+    const stepEl = document.querySelector('.bs-stepper');
+    this.stepper = new Stepper(stepEl!)
+  }
+
   ngOnInit() {
     this.currentUser = this.storageService.getUser();
     if (!this.currentUser.authenticated) {
       this.router.navigate(['/'])
     }
     this.canView = this.viewService.canView;
-    const stepEl = document.querySelector('.bs-stepper');
-    this.stepper = new Stepper(stepEl!)
   }
 
 }
